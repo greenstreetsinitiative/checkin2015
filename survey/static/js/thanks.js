3b9936cc00ccb8661356b77fe37200aeb55a5c1a
thecristen <cristen@outlook.com>
2015-04-30 19:21:14 -0400

    fix links to map

--- survey/static/js/thanks.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-  $('#checkin-another').on('click', function(event) {
-    event.preventDefault();
-    simpleStorage.deleteKey('commutersurvey');
-    window.location = '/commuterform';
-  });
-});
