b3a7e8146e2c651c7e02ebb9f25481b7a693ee90
thecristen <cristen@outlook.com>
2015-06-17 13:44:22 -0400

    Revert "start filtering companies by size"
    This reverts commit b0e667aea5a12bfef82208b5aaa846f7c73b25f4.

--- leaderboard/views.py
+++ leaderboard/views.py
@@ -84,38 +84,144 @@ def latest_leaderboard_subteams(request, parentid=None):
 
     return render_to_response('leaderboard/leaderboard_new.html', { 'ranks': ranks, 'totals': totals, 'request': request, 'employersWithSubteams': Employer.objects.filter(team__isnull=False).distinct(), 'employerName': Employer.objects.get(id=parentid) }, context)
 
-def latest_leaderboard(request, size='all', month=None):
+def latest_leaderboard(request):
     # Obtain the context from the HTTP request.
     context = RequestContext(request)
 
     d = {}
 
+    ### TODO - filter related commutersurveys by MONTH
+
     companies = Employer.objects.only('id','name').exclude(id__in=[32,33,34,38,39,40]).filter(
         active2015=True,
         commutersurvey__created__gte=datetime.date(2015, 04, 15),
-        commutersurvey__created__lte=datetime.date(2015, 11, 01))
-
-
-    ### TODO - filter related commutersurveys by MONTH
-    if month:
-        companies = companies.filter(commutersurvey__wr_day_id=month)
-
-    companies.annotate(
+        commutersurvey__created__lte=datetime.date(2015, 11, 01)).annotate(
         saved_carbon=Sum('commutersurvey__carbon_savings'),
         overall_calories=Sum('commutersurvey__calories_total'),
         num_checkins=Count('commutersurvey'))
 
-    if size == 'small':
-        companies = companies.filter(nr_employees__lte=50)
-    else:
-        if size == 'medium':
-            companies = companies.filter(nr_employees__gt=50,nr_employees__lte=300)
-        else:
-            if size == 'large':
-                companies = companies.filter(nr_employees__gt=300,nr_employees__lte=2000)
-            else:
-                if size == 'largest':
-                    companies = companies.filter(nr_employees__gt=2000)
+    totals = companies.aggregate(
+        total_carbon=Sum('saved_carbon'),
+        total_calories=Sum('overall_calories'),
+        total_checkins=Sum('num_checkins')
+    )
+
+    for company in companies:
+        d[str(company.name)] = calculate_metrics(company)
+
+    ranks = calculate_rankings(d)
+
+    return render_to_response('leaderboard/leaderboard_new.html', { 'ranks': ranks, 'totals': totals, 'request': request, 'employersWithSubteams': Employer.objects.filter(team__isnull=False).distinct() }, context)
+
+def latest_leaderboard_small(request):
+    # Obtain the context from the HTTP request.
+    context = RequestContext(request)
+
+    d = {}
+
+    ### TODO - filter related commutersurveys by MONTH
+
+    companies = Employer.objects.only('id','name').exclude(id__in=[32,33,34,38,39,40]).filter(
+        nr_employees__lte=50,
+        active2015=True,
+        commutersurvey__created__gte=datetime.date(2015, 04, 15),
+        commutersurvey__created__lte=datetime.date(2015, 11, 01)).annotate(
+        saved_carbon=Sum('commutersurvey__carbon_savings'),
+        overall_calories=Sum('commutersurvey__calories_total'),
+        num_checkins=Count('commutersurvey'))
+
+    totals = companies.aggregate(
+        total_carbon=Sum('saved_carbon'),
+        total_calories=Sum('overall_calories'),
+        total_checkins=Sum('num_checkins')
+    )
+
+    for company in companies:
+        d[str(company.name)] = calculate_metrics(company)
+
+    ranks = calculate_rankings(d)
+
+    return render_to_response('leaderboard/leaderboard_new.html', { 'ranks': ranks, 'totals': totals, 'request': request, 'employersWithSubteams': Employer.objects.filter(team__isnull=False).distinct()}, context)
+
+
+def latest_leaderboard_medium(request):
+    # Obtain the context from the HTTP request.
+    context = RequestContext(request)
+
+    d = {}
+
+    ### TODO - filter related commutersurveys by MONTH
+
+    companies = Employer.objects.only('id','name').exclude(id__in=[32,33,34,38,39,40]).filter(
+        nr_employees__gt=50,
+        nr_employees__lte=300,
+        active2015=True,
+        commutersurvey__created__gte=datetime.date(2015, 04, 15),
+        commutersurvey__created__lte=datetime.date(2015, 11, 01)).annotate(
+        saved_carbon=Sum('commutersurvey__carbon_savings'),
+        overall_calories=Sum('commutersurvey__calories_total'),
+        num_checkins=Count('commutersurvey'))
+
+    totals = companies.aggregate(
+        total_carbon=Sum('saved_carbon'),
+        total_calories=Sum('overall_calories'),
+        total_checkins=Sum('num_checkins')
+    )
+
+    for company in companies:
+        d[str(company.name)] = calculate_metrics(company)
+
+    ranks = calculate_rankings(d)
+
+    return render_to_response('leaderboard/leaderboard_new.html', { 'ranks': ranks, 'totals': totals, 'request': request, 'employersWithSubteams': Employer.objects.filter(team__isnull=False).distinct() }, context)
+
+def latest_leaderboard_large(request):
+    # Obtain the context from the HTTP request.
+    context = RequestContext(request)
+
+    d = {}
+
+    ### TODO - filter related commutersurveys by MONTH
+
+    companies = Employer.objects.only('id','name').exclude(id__in=[32,33,34,38,39,40]).filter(
+        nr_employees__gt=300,
+        nr_employees__lte=2000,
+        active2015=True,
+        commutersurvey__created__gte=datetime.date(2015, 04, 15),
+        commutersurvey__created__lte=datetime.date(2015, 11, 01)).annotate(
+        saved_carbon=Sum('commutersurvey__carbon_savings'),
+        overall_calories=Sum('commutersurvey__calories_total'),
+        num_checkins=Count('commutersurvey'))
+
+    totals = companies.aggregate(
+        total_carbon=Sum('saved_carbon'),
+        total_calories=Sum('overall_calories'),
+        total_checkins=Sum('num_checkins')
+    )
+
+    for company in companies:
+        d[str(company.name)] = calculate_metrics(company)
+
+    ranks = calculate_rankings(d)
+
+    return render_to_response('leaderboard/leaderboard_new.html', { 'ranks': ranks, 'totals': totals, 'request': request, 'employersWithSubteams': Employer.objects.filter(team__isnull=False).distinct() }, context)
+
+def latest_leaderboard_largest(request):
+    # Obtain the context from the HTTP request.
+    context = RequestContext(request)
+
+    d = {}
+
+    ### TODO - filter related commutersurveys by MONTH
+
+    companies = Employer.objects.only('id','name').exclude(id__in=[32,33,34,38,39,40]).filter(
+        nr_employees__gt=2000,
+        active2015=True,
+        commutersurvey__created__gte=datetime.date(2015, 04, 15),
+        commutersurvey__created__lte=datetime.date(2015, 11, 01)).annotate(
+        saved_carbon=Sum('commutersurvey__carbon_savings'),
+        overall_calories=Sum('commutersurvey__calories_total'),
+        num_checkins=Count('commutersurvey'))
 
     totals = companies.aggregate(
         total_carbon=Sum('saved_carbon'),
