47b6513e882bcbb88f136eb46f330d3842b00303
Gustavo Frankowiak <g.lfrankowiak@gmail.com>
2016-01-22 13:34:03 -0500

    added event model

--- retail/models.py
+++ retail/models.py
@@ -51,3 +51,56 @@ class partner(models.Model):
 
     def __unicode__(self):
         return u'%s' % (self.name)
+
+
+class event(models.Model):
+    # event info
+    name = models.TextField()
+    phone = models.CharField(max_length=32, null=True, blank=True)
+    website = models.CharField(max_length=2048, null=True, blank=True)
+    info = models.TextField()
+    date = models.DateTimeField(null=True)
+
+    # location
+    street = models.TextField()
+    city = models.TextField()
+    zipcode = models.CharField(max_length=6)
+    latitude = models.FloatField(default=0)
+    longitude = models.FloatField(default=0)
+
+    # Contact information
+    contact_name = models.TextField(null=True, blank=True)
+    contact_phone = models.TextField(null=True, blank=True)
+    contact_email = models.TextField(null=True, blank=True)
+
+    notes = models.TextField(null=True, blank=True)
+    approved = models.BooleanField(default=False)
+
+    def phoneNumber(self):
+        return '({0}) {1}-{2}'.format(self.phone[0:3], self.phone[3:6],
+                                      self.phone[6:])
+
+    def contactPhoneNumber(self):
+        if (self.contact_phone):
+            if len(self.contact_phone) == 10:
+                return '({0}) {1}-{2}'.format(self.contact_phone[0:3],
+                                              self.contact_phone[3:6],
+                                              self.contact_phone[6:10])
+            else:
+                return self.contact_phone
+        else:
+            return ''
+
+    def address(self):
+        return self.street + ', ' + self.city + ', MA ' + self.zipcode
+
+    def day(self):
+        day = self.date.date
+        return '{0}/{1}/{2}'.format(day.month, day.day, day.year)
+
+    def time(self):
+        time = self.date.time
+        return '{0}:{1}'.format(time.hour, time.minute)
+
+    def __unicode__(self):
+        return u'%s' % (self.name)
