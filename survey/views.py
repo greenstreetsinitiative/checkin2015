11031c5a444ec68ecf5884d46dde1d7b4664804a
thecristen <cristen@outlook.com>
2016-04-21 22:26:13 -0400

    comments, another validation check, adjust nesting for final if statement

--- survey/views.py
+++ survey/views.py
@@ -148,6 +148,9 @@ def add_checkin(request):
                                     leg = form.save(commit=False)
                                     leg.day = 'n'
                                     leg.direction = 'fw'
+                            else:
+                                # w/r from work is NOT VALID but needs to be.
+                                pass
                     else:
                         if request.POST['walkride_same_as_reverse'] == 'True':
                             # set w/r legs from work = w/r legs to work
@@ -173,10 +176,17 @@ def add_checkin(request):
                                     leg = form.save(commit=False)
                                     leg.day = 'n'
                                     leg.direction = 'fw'
+                            else:
+                                # normal to work is NOT VALID but needs to be.
+                                pass
                         else:
                             # IF THE WALKRIDE-SAME BUTTON IS "NO", OBTAIN USER INPUT FOR THAT SECTION
                             leg_formset_WRFW = MakeLegs_WRFW(
                                  request.POST, instance=commutersurvey, prefix='wfw')
+                            # validate wfw input
+                            for form in leg_formset_WRFW:
+                                if not form.is_valid():
+                                    all_legs_valid = False
                             # set normal legs to work = user input
                             leg_formset_NormalTW = MakeLegs_NormalTW(
                                  request.POST, instance=commutersurvey, prefix='ntw')
@@ -193,6 +203,9 @@ def add_checkin(request):
                                     leg = form.save(commit=False)
                                     leg.day = 'n'
                                     leg.direction = 'fw'
+                            else:
+                                # normal to work OR w/r from work is NOT VALID and needs to be
+                                pass
                 else:
                     if request.POST['normal_same_as_walkride'] == 'True':
                         if request.POST['walkride_same_as_reverse'] == 'True':
@@ -242,6 +255,9 @@ def add_checkin(request):
                                     leg = form.save(commit=False)
                                     leg.day = 'n'
                                     leg.direction = 'fw'
+                            else:
+                                # w/r from work is NOT VALID and needs to be
+                                pass
                     else:
                         if request.POST['walkride_same_as_reverse'] == 'True':
                             # set w/r legs from work = w/r legs to work
@@ -265,6 +281,10 @@ def add_checkin(request):
                             for form in leg_formset_NormalFW:
                                 if not form.is_valid():
                                     all_legs_valid = False
+
+                            if not all_legs_valid:
+                                # normal TW or normal FW are NOT VALID and need to be
+                                pass
                         else:
                             # IF THE WALKRIDE-SAME BUTTON IS "NO", OBTAIN USER INPUT FOR THAT SECTION
                             leg_formset_WRFW = MakeLegs_WRFW(
@@ -288,70 +308,80 @@ def add_checkin(request):
                                 if not form.is_valid():
                                     all_legs_valid = False
 
+                            if not all_legs_valid:
+                                # something is NOT VALID and needs to be
+                                pass
 
-            # finally! we're good to go.
-            if (all_legs_valid and
-                leg_formset_WRTW.is_valid() and
-                leg_formset_NormalTW.is_valid() and
-                leg_formset_NormalFW.is_valid() and
-                leg_formset_WRFW.is_valid()):
 
-                commutersurvey.save()
-                leg_formset_WRTW.save()
-                leg_formset_WRFW.save()
-                leg_formset_NormalTW.save()
-                leg_formset_NormalFW.save()
-                # very simple email sending - replace using Mandrill API later
-                name = commutersurvey.name or 'Supporter'
-                subject = ('Walk/Ride Day ' +
-                           commutersurvey.wr_day_month.month + ' Checkin')
-                message_html = (
-                    '<p>Dear {name},</p><p>Thank you for checking'
-                    ' in your Walk/Ride Day commute! This email confirms your'
-                    ' participation in {survey_date}\'s Walk/Ride Day! Feel '
-                    'free to show it to our <a href="http://checkin'
-                    '-greenstreets.rhcloud.com/retail" style="color:'
-                    '#2ba6cb;text-decoration: none;">Retail Partners</a> '
-                    'to take advantage of their offers of freebies, '
-                    'discounts, and other goodies!</p><p>If you haven\'t already, <a href="http://'
-                    'checkin2015-greenstreets.rhcloud.com/leaderboard/" '
-                    'style="color: #2ba6cb;text-decoration: none;">CLICK HERE'
-                    '</a> to see how your company did in the 2015 Corporate'
-                    ' Challenge, which ended with the October Walk/Ride Day.</p>'
-                    '<p>Thank you for being involved! Remember to check-in '
-                    'for next month\'s Walk/Ride Day.</p><p>Warmly,<br>'
-                    '<span style="color:#006600;font-weight:bold;">Janie Katz'
-                    '-Christy, Director <br>Green Streets Initiative<br> '
-                    '<span class="mobile_link">617-299-1872 (office)</p>'
-                    '<p>Share with your friends and colleagues! '
-                    '<a href="http://checkin.gogreenstreets.org" '
-                    'style="color: #2ba6cb;text-decoration: none;">Make sure'
-                    ' they get a chance to check in</p>'.format(
-                        name=name,
-                        survey_date=commutersurvey.wr_day_month.month))
+                # finally! we're good to go.
+                if (all_legs_valid and
+                    leg_formset_WRTW.is_valid() and
+                    leg_formset_NormalTW.is_valid() and
+                    leg_formset_NormalFW.is_valid() and
+                    leg_formset_WRFW.is_valid()):
 
-                message_plain = (
-                    'Dear Supporter, Thank you for checking in '
-                    'your Walk/Ride Day commute! This email confirms your'
-                    ' participation in ' + commutersurvey.wr_day_month.month +
-                    '\'s Walk/Ride Day! Feel free to show it to our Retail'
-                    ' Partners to take advantage of their offers of freebies,'
-                    ' discounts, and other goodies! Thank you for being'
-                    ' involved! Remember to check-in for next month\'s Walk/Ride'
-                    ' Day. Warmly, Green Streets Initiative')
-                recipient_list = [commutersurvey.email,]
-                from_email = 'checkin@gogreenstreets.org'
-                send_mail(subject, message_plain, from_email, recipient_list,
-                          html_message=message_html, fail_silently=True)
-                return render_to_response(
-                    'survey/thanks.html',
-                    {
-                        'person': commutersurvey.name,
-                        'calories_burned': commutersurvey.calories_total,
-                        'calorie_change': commutersurvey.calorie_change,
-                        'carbon_savings': commutersurvey.carbon_savings,
-                        'change_type': commutersurvey.change_type,
-                    })
+                    commutersurvey.save()
+                    leg_formset_WRTW.save()
+                    leg_formset_WRFW.save()
+                    leg_formset_NormalTW.save()
+                    leg_formset_NormalFW.save()
+                    # very simple email sending - replace using Mandrill API later
+                    name = commutersurvey.name or 'Supporter'
+                    subject = ('Walk/Ride Day ' +
+                               commutersurvey.wr_day_month.month + ' Checkin')
+                    message_html = (
+                        '<p>Dear {name},</p><p>Thank you for checking'
+                        ' in your Walk/Ride Day commute! This email confirms your'
+                        ' participation in {survey_date}\'s Walk/Ride Day! Feel '
+                        'free to show it to our <a href="http://checkin'
+                        '-greenstreets.rhcloud.com/retail" style="color:'
+                        '#2ba6cb;text-decoration: none;">Retail Partners</a> '
+                        'to take advantage of their offers of freebies, '
+                        'discounts, and other goodies!</p><p>If you haven\'t already, <a href="http://'
+                        'checkin2015-greenstreets.rhcloud.com/leaderboard/" '
+                        'style="color: #2ba6cb;text-decoration: none;">CLICK HERE'
+                        '</a> to see how your company did in the 2015 Corporate'
+                        ' Challenge, which ended with the October Walk/Ride Day.</p>'
+                        '<p>Thank you for being involved! Remember to check-in '
+                        'for next month\'s Walk/Ride Day.</p><p>Warmly,<br>'
+                        '<span style="color:#006600;font-weight:bold;">Janie Katz'
+                        '-Christy, Director <br>Green Streets Initiative<br> '
+                        '<span class="mobile_link">617-299-1872 (office)</p>'
+                        '<p>Share with your friends and colleagues! '
+                        '<a href="http://checkin.gogreenstreets.org" '
+                        'style="color: #2ba6cb;text-decoration: none;">Make sure'
+                        ' they get a chance to check in</p>'.format(
+                            name=name,
+                            survey_date=commutersurvey.wr_day_month.month))
+
+                    message_plain = (
+                        'Dear Supporter, Thank you for checking in '
+                        'your Walk/Ride Day commute! This email confirms your'
+                        ' participation in ' + commutersurvey.wr_day_month.month +
+                        '\'s Walk/Ride Day! Feel free to show it to our Retail'
+                        ' Partners to take advantage of their offers of freebies,'
+                        ' discounts, and other goodies! Thank you for being'
+                        ' involved! Remember to check-in for next month\'s Walk/Ride'
+                        ' Day. Warmly, Green Streets Initiative')
+                    recipient_list = [commutersurvey.email,]
+                    from_email = 'checkin@gogreenstreets.org'
+                    send_mail(subject, message_plain, from_email, recipient_list,
+                              html_message=message_html, fail_silently=True)
+                    return render_to_response(
+                        'survey/thanks.html',
+                        {
+                            'person': commutersurvey.name,
+                            'calories_burned': commutersurvey.calories_total,
+                            'calorie_change': commutersurvey.calorie_change,
+                            'carbon_savings': commutersurvey.carbon_savings,
+                            'change_type': commutersurvey.change_type,
+                        })
+                else:
+                    # it wasn't all valid
+                    pass
+            else:
+                # w/r to work is NOT VALID but needs to be.
+                pass
         else:
             pass
 
