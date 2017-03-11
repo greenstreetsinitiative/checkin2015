f47ee9a3027024a8e411108fc781d0f2fd718627
Ryan Ballenger <Ryan@Ryans-MacBook-Pro-5.local>
2017-03-10 19:48:52 -0500

    merged migrations

---
+++ survey/migrations/0011_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('survey', '0010_auto_20170128_2353'),
+        ('survey', '0010_employer_active2017'),
+    ]
+
+    operations = [
+    ]
