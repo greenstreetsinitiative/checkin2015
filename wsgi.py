443f12db61934ae04d5454405b4b28062c794c54
psmarsh <philip.s.marsahll@gmail.com>
2019-06-17 21:22:44 -0500

    remove unnecessary enivornmental variable

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@
 import os
 import checkin2015.wsgi
 
-virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+#virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 try:
     execfile(virtualenv, dict(__file__=virtualenv))
