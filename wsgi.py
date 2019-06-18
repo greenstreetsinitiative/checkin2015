7a516616984a670aefdb7c808542ed3ff29f6555
psmarsh <philip.s.marsahll@gmail.com>
2019-06-17 21:30:19 -0500

    comment out more stuff

--- wsgi.py
+++ wsgi.py
@@ -4,10 +4,10 @@ import checkin2015.wsgi
 
 #virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
 #virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-try:
-    execfile(virtualenv, dict(__file__=virtualenv))
-except IOError:
-    pass
+#try:
+#    execfile(virtualenv, dict(__file__=virtualenv))
+#except IOError:
+#    pass
 #
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
